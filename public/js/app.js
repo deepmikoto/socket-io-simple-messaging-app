15557170272b372254fb60872938aba92c611006
deepmikoto <deepmikoto.com>
2018-08-12 23:03:25 +0300

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,103 @@
+$(function () {
+    var socket;
+    
+    function initWSConnection() {
+        socket = io(window.location.origin + ':3000');
+        socket
+            .on('chat message', function (data) {
+                var $template = $($('#instant-message').html());
+                $template.find('#message').text(data.message);
+                $template.find('#time').text((new Date()).toLocaleDateString());
+                if (socket.id === data.socketId) {
+                    $template.addClass('own');
+                } else {
+                    $template.find('#username').text(data.username);
+                    playSound('definite')
+                }
+
+                $('#messages').append($template);
+                window.scrollTo(0, document.body.scrollHeight);
+            })
+            .on('user connected', function (username) {
+                $('#messages').append($('<li>').html('<b>' + username + '</b>' + ' has connected'));
+                window.scrollTo(0, document.body.scrollHeight);
+            })
+            .on('user disconnected', function (username) {
+                $('#messages').append($('<li>').html('<b>' + username + '</b>' + ' disconnected'));
+                window.scrollTo(0, document.body.scrollHeight);
+            })
+            .on('user typing start', function (username) {
+                if ($('#typing').find('li[data-username="' + username +'"]').length) {
+                    return;
+                }
+
+                $('#typing').append($('<li>').text(username + ' is typing').attr('data-username', username));
+            })
+            .on('user typing stop', function (username) {
+                $('#typing').find('li[data-username="' + username +'"]').remove();
+            });
+
+        return socket;
+    }
+
+    function bindFormSubmit() {
+        $('form').submit(function () {
+            if ($('#m').val()) {
+                socket.emit('chat message', $('#m').val());
+            }
+            $('#m').val('');
+
+            return false;
+        });
+    }
+    
+    function bindTypingAction() {
+        var timeout;
+        $('#m').on('keyup', function () {
+            clearTimeout(timeout);
+            socket.emit('user typing start', getUsername());
+            timeout = setTimeout(function () {
+                socket.emit('user typing stop', getUsername());
+            }, 1000);
+        })
+    }
+
+    function getUsername() {
+        var username,
+            cookies = document.cookie
+                .split('; ')
+                .map(function (cookieString) {
+                    var parts = cookieString.split(/=(.+)/),
+                        obj = {};
+                    obj[parts[0]] = parts[1];
+
+                    return obj;
+                })
+                .reduce(function (result, current) {
+                    return Object.assign(result, current);
+                });
+        username = cookies.username ||
+            function () {
+                return prompt('What is your username?') || 'anonymous'
+            }();
+        document.cookie = 'username=' + username;
+
+        return username;
+    }
+
+    /**
+     * @param {string} filename The name of the file WITHOUT ending
+     */
+    function playSound(filename){
+        document.getElementById("sound").innerHTML='' +
+            '<audio autoplay="autoplay"><source src="/audio/' + filename + '.mp3" type="audio/mpeg" />' +
+                '<source src="/audio/' + filename + '.ogg" type="audio/ogg" />' +
+                '<embed hidden="true" autostart="true" loop="false" src="/audio/' + filename +'.mp3" />' +
+            '</audio>';
+    }
+
+    getUsername();
+    socket = initWSConnection();
+    bindFormSubmit();
+    bindTypingAction();
+});
---
+++ websockets/server.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const io = require('socket.io')({
+    serveClient: false
+});
+
+io.on('connection', (client) => {
+    const username = getCookies(client.handshake.headers.cookie)['username'];
+    client.broadcast.emit('user connected', username);
+    console.log(username + ' connected');
+    client
+        .on('disconnect', () => {
+            client.broadcast.emit('user disconnected', username);
+            console.log(username + ' disconnected')
+        })
+        .on('chat message', msg => {
+            console.log(username + ': ' + msg);
+            io.emit('chat message', {
+                socketId: client.id,
+                username: username,
+                message: msg,
+            })
+        })
+        .on('user typing start', username => {
+            client.broadcast.emit('user typing start', username);
+        })
+        .on('user typing stop', username => {
+            client.broadcast.emit('user typing stop', username);
+        });
+});
+
+io.attach(3000);
+
+const getCookies = cookieString => cookieString
+    .split('; ')
+    .map((cookieString) => {
+        let parts = cookieString.split(/=(.+)/),
+            obj = {};
+        obj[parts[0]] = parts[1];
+
+        return obj;
+    })
+    .reduce((result, current) => Object.assign(result, current))
+    || {};
